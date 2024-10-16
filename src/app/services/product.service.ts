2e23f081eda74e47ddb13a41a24d2d27c4aebdb0
wobin1 <nathanielmusa3@gmail.com>
2024-10-16 08:32:06 +0100

    minor chnages

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,36 +7,52 @@ export class ProductService {
 
   products = [
     {
-      "name": "chiken",
-      "amountSold": 120,
+      "id": 1,
+      "warehouseId": 1,
+      "name": "Chicken",
+      "amountSold": 120
     },
     {
+      "id": 2,
+      "warehouseId": 2,
       "name": "Beef",
-      "amountSold": 120,
+      "amountSold": 120
     },
     {
-      "name": "catfish",
-      "amountSold": 120,
+      "id": 3,
+      "warehouseId": 3,
+      "name": "Catfish",
+      "amountSold": 120
     },
     {
+      "id": 4,
+      "warehouseId": 1,
       "name": "Tomatoes",
-      "amountSold": 120,
+      "amountSold": 120
     },
     {
+      "id": 5,
+      "warehouseId": 2,
       "name": "Flour",
-      "amountSold": 120,
+      "amountSold": 120
     },
     {
+      "id": 6,
+      "warehouseId": 3,
       "name": "Flour",
-      "amountSold": 120,
+      "amountSold": 120
     },
     {
+      "id": 7,
+      "warehouseId": 1,
       "name": "Yam",
-      "amountSold": 120,
+      "amountSold": 120
     },
     {
+      "id": 8,
+      "warehouseId": 2,
       "name": "Garlic",
-      "amountSold": 120,
+      "amountSold": 120
     }
   ]
 
